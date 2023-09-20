1e2c5bd4aa0147678ba727c743e5fcc8f9adf4c0
as1gh08 <116599123+as1gh08@users.noreply.github.com>
2023-09-20 16:47:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import pyautogui
 from resolution_optimizer import ResolutionOptimizer
 
 ro = ResolutionOptimizer((1920, 1280))
-pyautogui.moveTo(ro.calculate_current(pixel_cord=(200, 1000)))
+pyautogui.moveTo(ro.calculate_current(pixel_cord=(200, 1000))) #pixel_cord=(200, 1000) will find required pixel in other resolution
 
 
